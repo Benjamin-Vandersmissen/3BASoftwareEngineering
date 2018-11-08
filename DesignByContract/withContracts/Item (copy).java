117cc577c93d50a4a2d179b4fbad33167f823113
Frank-Jan <franciscus.fekkes@student.uantwerpen.be>
2018-11-08 15:30:04 +0100

    java files with ensures/requires/...

---
+++ DesignByContract/withContracts/Item (copy).java
@@ -0,0 +1,46 @@
+package Core;
+
+import java.util.HashSet;
+
+public class Item {
+	private int m_iid;
+	
+	private String m_name;
+	private String m_desc;
+	private float m_price;
+	private HashSet<String> m_category;
+	
+	/**
+	 * Create new Item
+	 * 
+	 * @param name
+	 * @param desc
+	 * @param price
+	 */
+	 //require name.size() > 0
+	 //require price > 0
+	 //where set category?? m_category never set??
+	public Item(String name, String desc, float price) {
+		
+	}
+	
+	public int ID() {
+		return m_iid;
+	}
+	
+	public String name() {
+		return m_name;
+	}
+	
+	public String desc() {
+		return m_desc;
+	}
+	
+	public float price() {
+		return m_price;
+	}
+	
+	public HashSet<String> category() {
+		return m_category;
+	}
+}
